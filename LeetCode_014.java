005e9f5a46d1cf1250dd5bc1049bc65ebccccd92
TTTTTong <t15529208705@gmail.com>
2019-03-22 00:13:06 +0800

    LeetCode_014,暴力法

---
+++ LeetCode_014.java
@@ -0,0 +1,33 @@
+package com.tongxy.LeetCode;
+
+import com.sun.deploy.util.StringUtils;
+
+public class LeetCode_014
+{
+    public static void main(String[] args)
+    {
+        String[] str = {"flower", "flow", "flirht"};
+
+        System.out.println(longestCommonPrefix(str));
+    }
+
+    public static String longestCommonPrefix(String[] strs)
+    {
+        if (strs.length == 0)
+        {
+            return "";
+        }
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+        {
+            while (strs[i].indexOf(prefix) != 0)
+            {
+                prefix = prefix.substring(0, prefix.length()-1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+}

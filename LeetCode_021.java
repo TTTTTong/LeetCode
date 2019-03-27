c65ebae9d528b1fc731074513e89fec2cc872745
TTTTTong <t15529208705@gmail.com>
2019-03-27 19:52:06 +0800

    LeetCode_021 递归解法

--- LeetCode_021.java
+++ LeetCode_021.java
@@ -7,38 +7,54 @@ public class LeetCode_021
 
     }
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2)
-    {
-        ListNode dummyHead = new ListNode(0);
-        ListNode cur = dummyHead;
+    // 遍历解法
+//    public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+//    {
+//        ListNode dummyHead = new ListNode(0);
+//        ListNode cur = dummyHead;
+//
+//        while (null != l1 && null != l2)
+//        {
+//            if (l1.val < l2.val)
+//            {
+//                cur.next = l1;
+//                cur = cur.next;
+//                l1 = l1.next;
+//            }
+//            else
+//            {
+//                cur.next = l2;
+//                cur = cur.next;
+//                l2 = l2.next;
+//            }
+//        }
+//
+//        if (null == l1)
+//        {
+//            cur.next = l2;
+//        }
+//        else
+//        {
+//            cur.next = l1;
+//        }
+//
+//        return dummyHead.next;
+//    }
 
-        while (null != l1 && null != l2)
-        {
-            if (l1.val < l2.val)
-            {
-                cur.next = l1;
-                cur = cur.next;
-                l1 = l1.next;
-            }
-            else
-            {
-                cur.next = l2;
-                cur = cur.next;
-                l2 = l2.next;
-            }
-        }
+    // 递归解法
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
 
-        if (null == l1)
-        {
-            cur.next = l2;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        else
-        {
-            cur.next = l1;
-        }
-
-        return dummyHead.next;
     }
+
 }
 
 class ListNode
